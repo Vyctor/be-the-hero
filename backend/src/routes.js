60429dd51a8ded67eaf022e1740d938ef2770c3d
Vyctor <vyctorguimaraes@gmail.com>
2020-03-27 00:23:33 -0300

    Adicionando parte backend

---
+++ backend/src/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+// Controllers
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileControler = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileControler.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;
